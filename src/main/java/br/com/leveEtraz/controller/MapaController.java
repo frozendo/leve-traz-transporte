17c3d0b1dc6dbab6cf2202a120df184cdd27612b
frozendo <fla_system@hotmail.com>
2015-09-16 22:41:06 -0300

    Criando classes de service e dao

--- src/main/java/br/com/leveEtraz/controller/MapaController.java
+++ src/main/java/br/com/leveEtraz/controller/MapaController.java
@@ -1,15 +1,23 @@
 package br.com.leveEtraz.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.leveEtraz.entity.Mapa;
+import br.com.leveEtraz.service.IMapaService;
+
 @RestController
 public class MapaController {
 
+	@Autowired
+	private IMapaService mapaService;
+
 	@RequestMapping(value = "/teste", method = RequestMethod.POST)
-	public void teste() {
-		System.out.print("Teste WebService");
+	public void teste(@RequestBody Mapa mapa) {
+		mapaService.save(mapa);
 	}
 
 	@RequestMapping(value = "/teste", method = RequestMethod.GET)
